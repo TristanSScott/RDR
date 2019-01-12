8dedf7a1da7d5646e55cc840a9841c85b41b53a8
Tristan Scott <tristan.s.scott@gmail.com>
2019-01-11 22:58:44 -0500

    more changes to flask server

--- PiRoot/usr/lib/raspiwifi/configuration_app/second_rest.py
+++ PiRoot/usr/lib/raspiwifi/configuration_app/second_rest.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 from time import sleep
-import first_servo
+import StepperMotorParallel2
 #import servo1.py
 import sys
 
@@ -13,36 +13,28 @@ app = Flask("Yeehaw")
 def hello():
     return "Hello World!"
 
-@app.route("/Servo")
+@app.route("/Motor")
 def servohome():
     return "What do you want to do with the servo? min | mid | max!"	
 
-@app.route('/Servo/max', methods=['GET'])
-def servomax():
+@app.route('/Motor/open', methods=['GET'])
+def motoropen():
 	try:
-		first_servo.max()
+		StepperMotorParallel2.open()
 		return jsonify(funny = "bob")
 	except (IndexError, IOError) as e:
 		return jsonify({'error': e.message}), 503
 
 
-@app.route('/Servo/mid', methods=['GET'])
-def servomid():
+@app.route('/Motor/close', methods=['GET'])
+def motorclose():
 	try:
-		first_servo.mid()
+		StepperMotorParallel2.close()
 		return jsonify(funny = "bob")
 
 	except (IndexError, IOError) as e:
 		return jsonify({'error': e.message}), 503
 
-@app.route('/Servo/min', methods=['GET'])
-def servomin():
-	try:
-		first_servo.min()
-		return jsonify(funny = "bob")
-
-	except (IndexError, IOError) as e:
-		return jsonify({'error': e.message}), 503
 
 # @app.route('/LED', methods=['GET'])
 # def get_data():
